59f62b951077fbd6f785cb179a26ca1bf49256b9
= <learningsjr@gmail.com>
2025-11-18 10:06:25 +0530

    root base added in vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
